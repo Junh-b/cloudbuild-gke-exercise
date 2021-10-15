0504c5d33b8cc0d3f3413064989da2eb989e4fe4
Junh <jooha208@gmail.com>
2021-10-15 11:15:49 +0900

    initial testing

---
+++ src/main/java/net/junhabaek/cloudbuild/CloudbuildApplication.java
@@ -0,0 +1,13 @@
+package net.junhabaek.cloudbuild;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudbuildApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudbuildApplication.class, args);
+    }
+
+}
---
+++ src/test/java/net/junhabaek/cloudbuild/CloudbuildApplicationTests.java
@@ -0,0 +1,13 @@
+package net.junhabaek.cloudbuild;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CloudbuildApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
